f8d4339a74b31c8555137ddb20a5150a54cc61fe
Munawar Din <munawardin856@gmail.com>
2026-01-04 12:17:17 +0500

    New Function added

--- script.js
+++ script.js
@@ -1,66 +1,75 @@
+let amount = document.querySelector(".amount input");
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
 const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/";
+  let fromCurr = document.querySelector(".from select");
+  let toCurr = document.querySelector(".to select");
+  let msg = document.querySelector(".msg")
 
-const dropdowns = document.querySelectorAll(".dropdown select")
-let fromCurrency = document.querySelector(".from select")
-let toCurrency = document.querySelector(".to select")
-let amountVal = document.querySelector(".amount input")
-let msg = document.querySelector(".msg")
-let btn = document.querySelector("form button")
-
-
-
-
-  const convert=async()=>{
-    let amount = amountVal.value;
-    const  URL = `${BASE_URL}${fromCurrency.value.toLowerCase()}.json`
-     console.log(URL)
-    let apiResponse = await fetch(URL);
-    let data = await apiResponse.json();
-    let rate = data[fromCurrency.value.toLowerCase()][toCurrency.value.toLowerCase()]
-    console.log(rate);
-    let result = amount*rate;
-    let finalResult = result.toFixed(2);
-    msg.innerText= `${amount} ${fromCurrency.value} = ${finalResult} ${toCurrency.value}`
-  }
-
-
-
-
-for (let select of dropdowns){
-     for (let code in countryList){
-      
-      let newOption = document.createElement("option")
-      newOption.innerText= code;
-      newOption.value=code;
-      if(select.id==="from" && code === "USD"){
-        newOption.selected="selected"
-      }
-      else if(select.id==="to" && code === "PKR"){
-        newOption.selected="selected"
-      }
-        select.append(newOption);
-     }
-     select.addEventListener("change",(evt)=>{
-      evt.preventDefault();
-      updateFlag(select)
-      convert();
-
-     })
-   convert();
+for(let select of dropdowns){
+    for(let code in countryList){
+        console.log(code)
+    let newOption = document.createElement("option")
+    newOption.innerText = code;
+    newOption.value=code;
+    if(select.id==="from" && code ==="USD"){
+        newOption.selected = "selected";
+    }
+    else if(select.id==="to" && code === "PKR"){
+        newOption.selected = "selected"
+     
+    }
+     
+    select.append(newOption); 
+     
 }
 
+const updateFlag=((element)=>{
+let currCode =  element.value;
+console.log("Currency Code: ",currCode)
+let countryCode =countryList[currCode]
+console.log("Country Code: ",countryCode)
+let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+let img= element.parentElement.querySelector("img");
+img.src =newSrc;
 
-
- const  updateFlag=(element)=>{
-    let currVal = element.value
-    let countryCode = countryList[currVal]
-      let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
-      let img = element.parentElement.querySelector("img")
-      img.src = newSrc;
-  }
-
-  
-  btn.addEventListener("click",(evt)=>{
-    evt.preventDefault();
+})
+select.addEventListener("change",(evt)=>{
+    updateFlag(evt.target);
+    amount.value = 1;
     convert();
-  })
+})
+
+ updateFlag(select);
+ 
+    
+}
+
+async function convert() {
+  
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+
+  let response = await fetch(URL);
+  let data = await response.json();
+
+
+let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+  let amountVal = amount.value;
+  let finalVal = amount.value*rate;
+  let finalResult = finalVal.toFixed(2);
+  msg.innerText = `${amountVal} ${fromCurr.value} = ${finalResult} ${toCurr.value}`
+}
+
+amount.addEventListener("input",(evt)=>{
+  convert();
+
+});
+btn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  convert();
+})
+ window.addEventListener("load", () => {
+      amount.value = 1;
+      convert();
+    });
+
